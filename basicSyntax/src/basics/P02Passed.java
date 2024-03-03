d8518408a6506d2f3ff4cc8786962d4179a32750
nikola.ninov <momzzze@abv.bg>
2024-03-03 20:00:27 +0200

    start exercises

---
+++ basicSyntax/src/basics/P02Passed.java
@@ -0,0 +1,14 @@
+package basics;
+
+import java.util.Scanner;
+
+public class P02Passed {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+
+        double number=Double.parseDouble(scanner.nextLine());
+        if(number>=3){
+            System.out.println("Passed!");
+        }
+    }
+}
