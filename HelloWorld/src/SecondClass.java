6cb907fefdbda8719d22b172568c8daf27cb774e
nikola.ninov <momzzze@abv.bg>
2024-03-06 20:04:18 +0200

    Update SecondClass.java

--- HelloWorld/src/SecondClass.java
+++ HelloWorld/src/SecondClass.java
@@ -30,5 +30,14 @@ public class SecondClass {
         if(isCar){
             System.out.println("This is not supposed to happen");
         }
+
+        String makeOfCar="Volkswagen";
+        boolean isDomestic=makeOfCar == "Volkswagen" ? false : true;
+
+        if(isDomestic){
+            System.out.println("This car is domestic to our country");
+        }
+        String s= (isDomestic)?"This car is domestic": "This car is not domestic";
+        System.out.println(s);
     }
 }
