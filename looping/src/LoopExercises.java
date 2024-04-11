dbaab42d0cef4d7b31c3a55b05ac442f553c7750
nikola.ninov <momzzze@abv.bg>
2024-04-11 09:56:56 +0300

    Update LoopExercises.java

--- looping/src/LoopExercises.java
+++ looping/src/LoopExercises.java
@@ -81,6 +81,15 @@ public class LoopExercises {
 //        numberToWords(0); // should print "Invalid Value" since the parameter is negative.
 //        numberToWords(1000); // should print "Invalid Value" since the parameter is negative.
 
+        canPack (1, 0, 4); // should return false since bigCount is 1 (big bag of 5 kilos) and goal is 4 kilos.
+        canPack (1, 0, 5); // should return true since bigCount is 1 (big bag of 5 kilos) and goal is 5 kilos.
+        canPack (0, 5, 4); //should return true since smallCount is 5 (small bags of 1 kilo) and goal is 4 kilos,
+        // and we have 1 bag left which is ok as mentioned above.
+        canPack (2, 2, 11); // should return true since bigCount is 2 (big bags 5 kilos each)
+        // and smallCount is 2 (small bags of 1 kilo), makes in total 12 kilos and goal is 11 kilos.
+        canPack (-3, 2, 12); // should return false since bigCount is negative.
+
+
     }
     public static boolean isEvenNumber(int num){
         return num%2==0;
@@ -287,5 +296,7 @@ public class LoopExercises {
         System.out.println();
     }
 
-
+    public static boolean canPack(int bigCount,int smallCount, int goal){
+        return false;
+    }
 }
